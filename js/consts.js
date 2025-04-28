d3f2e754872316b52b371930be54f897379289ec
Owen Cheesman <114047172+CheezlWeasel@users.noreply.github.com>
2025-04-28 20:14:18 +1000

    Un-Jankified

--- js/consts.js
+++ js/consts.js
@@ -1,6 +1,7 @@
 export const SITE_STYLE__CLASSIC = "classic";
-export const SITE_STYLE__ONE = SITE_STYLE__CLASSIC;
+export const SITE_STYLE__ONE = "one";
 
 export const SITE_STYLE_DISPLAY = {
-	[SITE_STYLE__CLASSIC]: "Classic (2014)"
+	[SITE_STYLE__CLASSIC]: "Classic (2014)",
+	[SITE_STYLE__ONE]: "Modern (2024)"
 };
